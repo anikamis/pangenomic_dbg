293ecfc2f2fa88b02b2cdaf988419f6faf250f24
anika misra <ani.misra26@gmail.com>
2022-12-09 19:54:20 -0500

    more user friendly options

--- pangenome.py
+++ pangenome.py
@@ -7,26 +7,112 @@ from edge_def import Edge as E
 from node_def import Node as N
 from graph_def import Graph as G
 
-def main():
-    files = sys.argv[1]
-    outfix = sys.argv[2]
-    k = int(sys.argv[3])
 
-    compress = True
+def main():
+    ret = parse_args(sys.argv[1:])
+
+    if ret is None:
+        return
+
+    files, json_outfix, k, compress, query, query_search = ret
 
     path = "/".join(files.split("/")[:-1])
     strainmap, graph, strains = build_all_files(path, files, k)
 
     if compress:
         graph, maxlen = get_non_branching(graph)
-    
+
     colors = generate_colors(strains)
     nodes, strains, links = graph_to_json(graph, colors, strainmap, k, maxlen)
-    write_json(nodes, strains, links, outfix)
-    print(len(nodes), len(links))
+    write_json(nodes, strains, links, json_outfix)
 
-#def get_unique_seqs(graph, strainmap, colors)
-    
+    if query:
+        unique = get_unique(query_search, graph, colors, strainmap)
+        graph_to_fasta(query_search, unique)
+
+
+def parse_args(args):
+    file_list = ""
+    json_outfix = ""
+    k = 0
+    compress = True
+    query = False
+    query_search = ""
+
+    try:
+        if args[0] == "-i":
+            file_list = args[1]
+        else:
+            return print_usage()
+
+        if args[2] == "-o":
+            json_outfix = args[3]
+        else:
+            return print_usage()
+
+        if args[4] == "-k":
+            k = int(args[5])
+        else:
+            return print_usage()
+
+        optargs = args[6:]
+
+        if len(optargs):
+            if optargs[0] == "-u":
+                compress = False
+                optargs = optargs[1:]
+
+            if optargs[0] == "-q":
+                query = True
+                query_search = optargs[1].strip()
+
+    except IndexError:
+        return print_usage()
+
+    return (file_list, json_outfix, k, compress, query, query_search)
+
+
+def print_usage():
+    print(
+        "USAGE: python3 pangenome.py -i <input file> -o <output file> -k <kmer_size> [-u] [-q <query>]")
+    return None
+
+
+def graph_to_fasta(strain, graph):
+    with open(strain + ".fasta", "w") as fp:
+        for i, n in enumerate(graph.nodes.keys()):
+            fp.write(">" + strain + "_unique_seq_" + i + "\n")
+            fp.write(n + "\n")
+
+
+def filter_graph(graph, color):
+    genome = G(dict(), list())
+    for n in graph.nodes.values():
+        if n.color == color:
+            genome.nodes[n.label] = n.copy()
+
+    for e in graph.edges:
+        if e.inn.color == color and e.outn.color == color:
+            genome.edges.append(e.copy())
+
+    return genome
+
+
+def get_unique(strains, graph, colors, strainmaps):
+    reverse = {v: k for k, v in strainmaps.items()}
+
+    try:
+        if strains == "core":
+            color = colors[frozenset({strainmaps.keys()})]
+        else:
+            color = colors[frozenset({reverse[strains]})]
+
+    except KeyError:
+        print("Strain not found")
+        return None
+
+    return filter_graph(graph, color)
+
 
 def graph_to_json(graph, colors, strainmap, k, maxlen):
 
@@ -45,22 +131,29 @@ def write_json(nodes, strains, links, outfix):
 def generate_colors(strains):
     colors = dict()
 
-    for strain in strains:
-        colors[strain] = "#%06x" % random.randint(0, 0xFFFFFF)
+    # ensure all colors are unique
+    all_colors = set(["#%06x" % random.randint(0, 0xFFFFFF)
+                     for i in range(len(strains))])
+
+    while len(colors) < len(strains):
+        all_colors.add("#%06x" % random.randint(0, 0xFFFFFF))
+
+    colors = dict(map(lambda i, j: (i, j), strains, list(all_colors)))
 
     return colors
+
 
 def strains_to_json(strainmap, colors):
     strains = []
 
     for idx, name in strainmap.items():
-        temp =  dict()
+        temp = dict()
         temp["id"] = idx
         temp["name"] = name
         temp["colors"] = [v for k, v in colors.items() if idx in k]
-        
+
         strains.append(temp)
-    
+
     return strains
 
 
@@ -69,7 +162,7 @@ def nodes_to_json(graph, colors, k, maxlen):
 
     # hardcode max/min pixel size to 100, 4
     # scale each node size by relative length of sequence label
-    scale = lambda x : int((x - k)/(maxlen - k) * (100 - 4) + 5)
+    def scale(x): return int((x - k)/(maxlen - k) * (100 - 4) + 5)
 
     for label, node in graph.nodes.items():
         temp = dict()
@@ -78,7 +171,7 @@ def nodes_to_json(graph, colors, k, maxlen):
         temp["strains"] = list(node.strains)
         temp["size"] = scale(len(label))
         nodes.append(temp)
-    return sorted(nodes, key=lambda x: len(x["strains"]))
+
     return nodes
 
 
@@ -100,27 +193,29 @@ def get_non_branching(graph):
     maxlen = 0
     for n_label, n in graph.nodes.items():
         if n.is_branching():
-            start = n.copy_in() if n_label not in compress.nodes else compress.nodes[n_label]
+            start = n.copy_in(
+            ) if n_label not in compress.nodes else compress.nodes[n_label]
             compress.nodes[n.label] = start
 
             for out_edge, out_node in n.outgoing.items():
 
                 curr_end = out_node
                 label = curr_end.label
-                
-                count = 0 
+
+                count = 0
 
                 while not curr_end.is_branching():
                     count += 1
 
                     next_edge, next_node = list(curr_end.outgoing.items())[0]
-                    
+
                     if not next_node.is_branching():
                         label += next_edge
 
                     curr_end = next_node
 
-                end = curr_end.copy_out() if curr_end.label not in compress.nodes else compress.nodes[curr_end.label]
+                end = curr_end.copy_out(
+                ) if curr_end.label not in compress.nodes else compress.nodes[curr_end.label]
                 compress.nodes[end.label] = end
 
                 if count == 0:
@@ -140,9 +235,10 @@ def get_non_branching(graph):
                     compress.edges.append(E(middle, end))
 
                     compress.nodes[middle.label] = middle
-                    maxlen = len(middle.label) if len(middle.label) > maxlen else maxlen
+                    maxlen = len(middle.label) if len(
+                        middle.label) > maxlen else maxlen
     return compress, maxlen
-                
+
 
 def build_graph_from_seq(strain, seq, k, graph=None):
     nodes = dict() if graph is None else graph.nodes
@@ -153,13 +249,15 @@ def build_graph_from_seq(strain, seq, k, graph=None):
         if (i + k) == len(seq):
             nodes[seq[i:i+k]].strains.add(strain)
             break
-    
+
         in_mer = seq[i:i+k]
         out_mer = seq[i+1:i+k+1]
         e_mer = out_mer[-1]
 
-        if in_mer not in nodes: nodes[in_mer] = N(in_mer)
-        if out_mer not in nodes: nodes[out_mer] = N(out_mer)
+        if in_mer not in nodes:
+            nodes[in_mer] = N(in_mer)
+        if out_mer not in nodes:
+            nodes[out_mer] = N(out_mer)
 
         in_node = nodes[in_mer]
 
@@ -182,8 +280,9 @@ def build_graph_from_seq(strain, seq, k, graph=None):
         if new_edge:
             edge = E(in_node, out_node)
             edges.append(edge)
-        
+
     return G(nodes, edges), strains
+
 
 def read_seq(infile):
     seq = np.str_("")
